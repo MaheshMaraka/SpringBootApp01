ae16bf133b6db3146ee0dfd9409c86f8db9d3335
MaheshBabu Maraka <maheshbabumaraka@gmail.com>
2021-03-11 10:26:21 +0530

    second commit

--- src/main/java/com/user/pojos/SpringBootApp01Application.java
+++ src/main/java/com/user/pojos/SpringBootApp01Application.java
@@ -8,6 +8,10 @@ public class SpringBootApp01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp01Application.class, args);
+		
+	}
+	public void modified() {
+		System.out.println("welcome to spring boot");
 	}
 
 }
